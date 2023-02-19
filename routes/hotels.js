806eb46ef7f348ffee1ba87e1852afd1f643d352
AlexBergDev <68918064+AlexBergDev@users.noreply.github.com>
2023-02-19 16:57:32 +0100

    Added hotel details and rating functionality

--- routes/hotels.js
+++ routes/hotels.js
@@ -12,11 +12,24 @@ router.get("/", async function (req, res, next) {
   res.render("hotels", { title: "Hotels", hotels: hotels });
 });
 
+/* GET hotel details. */
+router.get("/:hotelId", async function (req, res, next) {
+  const hotel = await hotelService.getHotelDetails(req.params.hotelId);
+  res.render("hotelDetails", { hotel: hotel });
+});
+
 /* POST hotels listing. */
 router.post("/", jsonParser, async function (req, res, next) {
   let Name = req.body.Name;
   let Location = req.body.Location;
   await hotelService.create(Name, Location);
+  res.end();
+});
+
+/* POST hotel rating. */
+router.post("/:hotelId/rate", jsonParser, async function (req, res, next) {
+  let value = req.body.Value;
+  await hotelService.makeARate(1, req.params.hotelId, value);
   res.end();
 });
 
--- services/HotelService.js
+++ services/HotelService.js
@@ -2,6 +2,8 @@ class HotelService {
   constructor(db) {
     this.client = db.sequelize;
     this.Hotel = db.Hotel;
+    this.Rate = db.Rate;
+    this.User = db.User;
     console.log(db);
   }
 
@@ -15,6 +17,35 @@ class HotelService {
   async get() {
     return this.Hotel.findAll({
       where: {},
+    });
+  }
+
+  async getHotelDetails(hotelId) {
+    const hotel = await this.Hotel.findOne({
+      where: {
+        id: hotelId,
+      },
+      include: {
+        model: this.User,
+        through: {
+          attributes: ["Value"],
+        },
+      },
+    });
+    hotel.avg =
+      hotel.Users.map((x) => x.Rate.dataValues.Value).reduce(
+        (a, b) => a + b,
+        0
+      ) / hotel.Users.length;
+    hotel.rated = hotel.Users.filter((x) => x.dataValues.id == 1).length > 0;
+    return hotel;
+  }
+
+  async makeARate(userId, hotelId, value) {
+    return this.Rate.create({
+      UserId: userId,
+      HotelId: hotelId,
+      Value: value,
     });
   }
 
